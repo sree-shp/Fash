209f234449c509406a3d10ce3bd95de1eb8649b1
Sreehari <sree2512k@gmail.com>
2023-09-04 15:13:47 +0530

    change localhost

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(cors(
   {
-    origin: "http://localhost:3000",
+    origin: "https://fash-store.vercel.app",
     credentials: true
   }
 ));
