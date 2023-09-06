c3e2df70367cfd9529f492f22763b9c10baf4e1d
Sreehari <sree2512k@gmail.com>
2023-09-06 14:03:44 +0530

    change urls

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(cors(
   {
-    origin: "https://fash-store.vercel.app",
+    origin: "https://fash-store.vercel.app/",
     credentials: true
   }
 ));
